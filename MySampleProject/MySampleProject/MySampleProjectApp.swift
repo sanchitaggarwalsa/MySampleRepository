f15ec6e666909ac25cd648d2fa00ac00a03b9ffa
Sanchit <sanchitaggarwalsa9@icloud.com>
2020-10-13 15:56:18 -0400

    Add files via upload

---
+++ MySampleProject/MySampleProject/MySampleProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySampleProjectApp.swift
+//  MySampleProject
+//
+//  Created by Sanchit Aggarwal on 2020-10-13.
+//
+
+import SwiftUI
+
+@main
+struct MySampleProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
